def23bc48a74611033a4fc01a0ac05159ed45415
zzzzzhy <11657924@qq.com>
2023-12-28 14:05:56 +0800

    fix:ping结果打印到logread

--- hi/constants.go
+++ hi/constants.go
@@ -2746,7 +2746,7 @@ fi
 cat >/tmp/net_ping_detected<<EOF
 node_host={{.nodeHost}}
 import_ip=\$(uci get wireguard.@peers[0].end_point|awk -F':' '{print \$1}')
-oping -c25 -i 2 -w 2 -O /var/log/ping.log \$import_ip \$node_host 8.8.8.8 |grep 'timeout' |logger -t ping
+oping -c25 -i 2 \$import_ip \$node_host 8.8.8.8 |grep 'timeout' |logger -t ping
 EOF
 host="{{.logUrl}}/$(uci get rtty.general.id)$(_sign)"
 dmesg >/var/log/dmesg.log
